e9e95ddbb32fe024f154b5362bc89e35cc808e25
Lucas <80749221+LucasCGG@users.noreply.github.com>
2022-12-22 10:41:41 +0100

    updates

--- src/Pages/User.js
+++ src/Pages/User.js
@@ -6,21 +6,27 @@ class User extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-
       user: {
         email: '',
         name: '',
         username: '',
         password: '',
-      }
+        rank: '',
+      },
+      userlogin: {
+        usernameOrEmail: '',
+        password: '',
+      },
     }
-    this.handleEmail = this.handleEmail.bind(this);
-    this.handleName = this.handleName.bind(this);
-    this.handleUsername = this.handleUsername.bind(this);
-    this.handlePasswort = this.handlePasswort.bind(this);
-    this.handleSubmitChangeLogin = this.handleSubmitChangeLogin.bind(this);
-    this.handleSubmitChangeRegister = this.handleSubmitChangeRegister.bind(this);
-    this.handleUsernameOrEmail = this.handleUsernameOrEmail.bind(this);
+    this.handleEmail = this.handleEmail.bind(this)
+    this.handleName = this.handleName.bind(this)
+    this.handleUsername = this.handleUsername.bind(this)
+    this.handlePasswort = this.handlePasswort.bind(this)
+    this.handleUsernameOrEmail = this.handleUsernameOrEmail.bind(this)
+    this.handleRank = this.handleRank.bind(this)
+
+    this.handleSubmitChangeLogin = this.handleSubmitChangeLogin.bind(this)
+    this.handleSubmitChangeRegister = this.handleSubmitChangeRegister.bind(this)
   }
 
   componentDidMount() {
@@ -29,7 +35,7 @@ class User extends React.Component {
       username: this.props.appState.name,
       rank: this.props.appState.rank,
     })
-    this.swap();
+    this.swap()
 
     /**
      * Element to be changed come here ("titles", "rank-text" etc.)
@@ -41,61 +47,88 @@ class User extends React.Component {
    * Prefferably with a login fom
    */
 
-  
-
-  handleUsername(event){
-    this.setState({user:{username: event.target.value }});
-  }
-  handleUsernameOrEmail(event){
-    this.setState({usernameOrEmail: event.target.value });
-  }
-  handleName(event){
-    this.setState({user:{name : event.target.value }});
-  }
-  handleEmail(event){
-    this.setState({user:{email: event.target.value }});
-  }
-  handlePasswort(event){
-    this.setState({user:{password: event.target.value }});
-  }
-  handleSubmitChangeRegister(event){
-    // IDEA: ALERT FOR WHEN USER IS CREATED SUCCESSFULLY
-
-    event.preventDefault();
-
-    const requestOptions = {
-      method: "POST",
-      headers: {"Content-Type": "application/json"},
-      body: JSON.stringify(this.state.user)
-    }
-    fetch("http://127.0.0.1:8080/api/auth/signup", requestOptions)
-    .then(response => response.json())
-    .then(json => {
-      var jsonAnsw = JSON.stringify(json);
-      if(json.status === 200){
-      alert("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-      }
-      alert(jsonAnsw);
+  handleUsername(event) {
+    this.setState({
+      user: {
+        username: event.target.value,
+      },
     })
   }
-  handleSubmitChangeLogin(event){
-    const requestOptionsPost =  {
-      method:"POST",
-      headers: {"Content-Type":"application/json"},
-      body: JSON.stringify(this.state.email, this.state.username, this.state.password)
+  handleUsernameOrEmail(event) {
+    this.setState({ usernameOrEmail: event.target.value })
+  }
+  handleName(event) {
+    this.setState({
+      user: {
+        name: event.target.value,
+      },
+    })
+  }
+  handleEmail(event) {
+    this.setState({
+      user: {
+        email: event.target.value,
+      },
+    })
+  }
+  handlePasswort(event) {
+    this.setState({
+      user: {
+        password: event.target.value,
+      },
+    })
+  }
+
+  handleRank(event){
+    this.setState({
+      user:{
+        rank: event.target.value,
+      }
+    })
+  }
+
+
+  handleSubmitChangeRegister(event) {
+    // IDEA: ALERT FOR WHEN USER IS CREATED SUCCESSFULLY
+
+    event.preventDefault()
+
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(this.state),
+    }
+    fetch('http://127.0.0.1:8080/api/auth/signup', requestOptions)
+      .then((response) => response)
+      .then((json) => {
+        if (json.status === 200) {
+          alert(
+            'Your Account was successfully created. Pls continue to the Login.',
+          )
+        } else if (json.status === 500) {
+          alert('Internal Server Error. Try again or contact the support')
+        }
+      })
+  }
+  handleSubmitChangeLogin(event) {
+    const requestOptionsPost = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(this.state.user),
     }
 
     const requestOptionsGet = {
-      method:"GET",
-      headers:{"Content-Type":"application/json"},
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
     }
 
-    fetch("http://127.0.0.1:8080/api/auth/signin", requestOptionsPost)
-    .then(response => response)
-    .then(json =>{
-      var jsonAnsw = JSON.stringify(json);
-      if(json.status === 200){
-        alert(jsonAnsw);
+    fetch('http://127.0.0.1:8080/api/auth/signin', requestOptionsPost)
+      .then((response) => response)
+      .then((json) => {
+        var jsonAnsw = JSON.stringify(json)
+        if (json.status === 200) {
+          alert(jsonAnsw)
+          /* FETCH TO GET USER DATA AFTER LOGIN
         fetch("http://127.0.0.1:8080/user/getByEmail="+this.state.email, requestOptionsGet)
         .then(response => response.json())
         .then(json => {
@@ -106,17 +139,17 @@ class User extends React.Component {
             name:json.name,
             email:json.email
           })
-        })
-      }
-    })
+        })*/
+        }
+      })
   }
 
-  resetForm(){
+  resetForm() {
     this.setState({
       email: '',
       name: '',
       username: '',
-      password: ''   
+      password: '',
     })
   }
   swap = () => {
@@ -153,18 +186,33 @@ class User extends React.Component {
                   Login
                   <span class="underline"></span>
                 </button>
-                <form class="form form-login" onSubmit={this.handleSubmitChangeLogin}>
+                <form
+                  class="form form-login"
+                  onSubmit={this.handleSubmitChangeLogin}
+                >
                   <fieldset>
                     <legend>
                       Please, enter your email and password for login.
                     </legend>
                     <div class="input-block">
                       <label for="login-email">E-Mail or Username</label>
-                      <input id="login-email" type="text" required value={this.state.usernameOrEmail} onChange={this.handleUsernameOrEmail}/>
+                      <input
+                        id="login-email"
+                        type="text"
+                        required
+                        value={this.state.userlogin.usernameOrEmail}
+                        onChange={this.handleUsernameOrEmail}
+                      />
                     </div>
                     <div class="input-block">
                       <label for="login-password">Password</label>
-                      <input id="login-password" type="password" required value={this.state.password} onChange={this.handlePasswort}/>
+                      <input
+                        id="login-password"
+                        type="password"
+                        required
+                        value={this.state.userlogin.password}
+                        onChange={this.handlePasswort}
+                      />
                     </div>
                   </fieldset>
                   <button type="submit" class="btn-login">
@@ -177,29 +225,75 @@ class User extends React.Component {
                   Sign Up
                   <span class="underline"></span>
                 </button>
-                <form class="form form-signup" onSubmit={this.handleSubmitChangeRegister}>
+                <form
+                  class="form form-signup"
+                  onSubmit={this.handleSubmitChangeRegister}
+                >
                   <fieldset>
                     <legend>
-                      Please, enter your email, password and password
-                      confirmation for sign up.
+                      Please, enter your email, username, name and password for
+                      sign up.
                     </legend>
                     <div class="input-block">
                       <label for="signup-email">E-Mail</label>
-                      <input id="signup-email" type="email" required value={this.state.user.email} onChange={this.handleEmail} />
+                      <input
+                        id="signup-email"
+                        type="email"
+                        required
+                        value={this.state.user.email}
+                        onChange={this.handleEmail}
+                      />
                     </div>
                     <div class="input-block">
                       <label for="signup-username">Username</label>
-                      <input id="signup-username" type="text" required value={this.state.user.username} onChange={this.handleUsername}/>
+                      <input
+                        id="signup-username"
+                        type="text"
+                        required
+                        value={this.state.user.username}
+                        onChange={this.handleUsername}
+                      />
                     </div>
                     <div class="input-block">
                       <label for="signup-name">Name</label>
-                      <input id="signup-name" type="text" required value={this.state.user.name} onChange={this.handleName}/>
+                      <input
+                        id="signup-name"
+                        type="text"
+                        required
+                        value={this.state.user.name}
+                        onChange={this.handleName}
+                      />
                     </div>
                     <div class="input-block">
                       <label for="signup-password">Password</label>
-                      <input id="signup-password" type="password" required value={this.state.user.password} onChange={this.handlePasswort}/>
+                      <input
+                        id="signup-password"
+                        type="password"
+                        required
+                        value={this.state.user.password}
+                        onChange={this.handlePasswort}
+                      />
                     </div>
-
+                    <div class="input-block">
+                      <label for="Coach">Coach</label>
+                      <input
+                        name="select-rank"
+                        id="Coach"
+                        type="radio"
+                        required
+                        value="USER_OWNER"
+                        onSelect={this.handleRank}
+                      />
+                      <label for="Anbieter">Typ</label>
+                      <input
+                        name="select-rank"
+                        id="Anbieter"
+                        type="radio"
+                        required
+                        value="USER_COACH"
+                        onSelect={this.handleRank}
+                      />
+                    </div>
                   </fieldset>
                   <button type="submit" class="btn-signup">
                     Continue
