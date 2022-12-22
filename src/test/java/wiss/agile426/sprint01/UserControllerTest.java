bf43f56c4762934679416b22a4c6ba522124c0f0
thisisvv <thisisvv@gmail.com>
2022-12-22 10:46:38 +0100

    + Mutli role support -> add new roles to db: USER_OWNER + USER_COACH

--- src/test/java/wiss/agile426/sprint01/UserControllerTest.java
+++ src/test/java/wiss/agile426/sprint01/UserControllerTest.java
@@ -1,34 +1,47 @@
 package wiss.agile426.sprint01;
 
-
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.context.annotation.Bean;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import wiss.agile426.sprint01.controller.UserController;
-import org.springframework.test.web.servlet.MockMvc;
-import wiss.agile426.sprint01.repository.UserRepository;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(SpringExtension.class)
-@WebMvcTest
-@AutoConfigureMockMvc
-@ContextConfiguration(classes = UserController.class)
-public class UserControllerTest {
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class UserControllerTest extends GlobalMethodSecurityConfiguration {
 
-    @Autowired private UserController userController;
 
-    @Autowired private UserRepository userRepository;
+    @Autowired
+    private WebApplicationContext context;
 
-    @Autowired private MockMvc mockMvc;
+    private MockMvc mvc;
 
+    @Before
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+    // ... other methods
+
+    @WithMockUser("spring")
     @Test
-    public void CheckLanguageControllerInject_NotNull() throws Exception {
-        assertThat(userController).isNotNull();
+    public void givenAuthRequestOnPrivateService_shouldSucceedWith200() throws Exception {
+        mvc.perform(get("/private/hello").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
 }
+
