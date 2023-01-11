46d25a8946d2337dd5d7bf092ecd340761e828ad
Lucas <80749221+LucasCGG@users.noreply.github.com>
2023-01-11 11:47:33 +0100

    Updated MOdal
    Modal now closes and has Inputs
    All fetches still remaining

--- src/Pages/functiones/ModalDialog.js
+++ src/Pages/functiones/ModalDialog.js
@@ -1,14 +1,27 @@
 import React from 'react'
 import { Modal, Button } from 'react-bootstrap'
+
+import '../../Styles/modal.css'
+
 function ModalDialog() {
   const [isShow, invokeModal] = React.useState(false)
   const initModal = () => {
     return invokeModal(!false)
   }
+  const closeModal = () => {
+    return invokeModal(false)
+  }
+  const addProject = () => {
+    /**
+     * FETCH TO ADD PROJECT
+     */
+
+    return invokeModal(false)
+  }
   return (
     <>
-      <Button variant="success" onClick={initModal}>
-        Open Modal
+      <Button variant="dark" onClick={initModal}>
+        Add new Project
       </Button>
       <Modal show={isShow}>
         <Modal.Header closeButton onClick={initModal}>
@@ -16,16 +29,24 @@ function ModalDialog() {
         </Modal.Header>
         <Modal.Body>
           <form>
-            <label for="name">Project Name:</label><br />
-            <input type="text" id="name" name="name"/> <br />
+            <div>
+              <label for="name">Project Name:</label>
+              <input type="text" id="name" name="name" />
+            </div>
+            <div>
+              <label for="date">Datum:</label>
+              <input type="date" id="date" name="date" />
+            </div>
+            <label for="desc">Description:</label>
+            <textarea id="desc" name="desc" />
           </form>
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="danger" onClick={initModal}>
-            Close
+          <Button variant="success" onClick={addProject}>
+            Add Project
           </Button>
-          <Button variant="dark" onClick={initModal}>
-            Store
+          <Button variant="danger" onClick={closeModal}>
+            Cancel
           </Button>
         </Modal.Footer>
       </Modal>
